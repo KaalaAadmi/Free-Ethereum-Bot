7cc6ff52b527473edf1ab95646910833fafcb065
KaalaAadmi <51954157+KaalaAadmi@users.noreply.github.com>
2021-02-09 03:06:16 +0530

    Updated Version
    Version Update

--- Ethereum.py
+++ Ethereum.py
@@ -5,7 +5,7 @@ import time
 
 username = "<Enter your email ID>"
 password = "<Enter your password>"
-driver = webdriver.Edge(executable_path=r'<Path to your web driver>')
+driver = webdriver.<Browser Name>(executable_path=r'<Path to your web driver>')
 time.sleep(5)
 driver.get("https://freeethereum.com/free")
 
@@ -13,15 +13,17 @@ time.sleep(10)
 
 username = driver.find_element_by_xpath('/html/body/main/section/section[1]/div/div/div[2]/div/div[1]/div[1]/input').send_keys(username)
 password = driver.find_element_by_xpath('/html/body/main/section/section[1]/div/div/div[2]/div/div[1]/div[2]/input').send_keys(password)
-cross = driver.find_element_by_xpath('/html/body/div[3]/span').click()
+driver.fullscreen_window()
 loginBtn = driver.find_element_by_xpath('/html/body/main/section/section[1]/div/div/div[2]/div/div[1]/button').click()
 time.sleep(7)
+driver.fullscreen_window()
 while 1:
+    time.sleep(10)
+    driver.switch_to.window(driver.current_window_handle)
     roll = driver.find_element_by_xpath('/html/body/main/div/div/div/div/div/div[5]/button').click()
     print('1 hour wait starts now')
-    time.sleep(10)
-    survey_cross = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/span').click()
+    time.sleep(15)
+    driver.refresh()
     time.sleep(60*60)
     time.sleep(10)
     driver.refresh()
-    time.sleep(10)
